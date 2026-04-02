c8605cce15f393d82b97f05689d07f406bf7528b
Sneha-Deepthi <322103310097@gvpce.ac.in>
2026-04-02 13:13:59 +0530

    Token expiry Changed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '7h'
+      expiresIn: '3d'
     });
 
     res.json({
