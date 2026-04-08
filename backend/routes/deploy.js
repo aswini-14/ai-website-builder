e431f0e2ebac10367a23307ba170ee18f201752d
Sneha-Deepthi <322103310097@gvpce.ac.in>
2026-04-08 17:56:20 +0530

    Fix

--- backend/routes/deploy.js
+++ backend/routes/deploy.js
@@ -22,7 +22,7 @@ router.post("/:id", authMiddleware, async (req, res) => {
 
     res.json({
       message: "Project deployed successfully",
-      url: `${BACKEND_URL}/site/${project._id}`
+      url: `${process.env.BACKEND_URL}/site/${project._id}`
     });
 
   } catch (err) {
